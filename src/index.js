2add9dad48c70dbb1b2ef1807efbe16fe983ae1d
Tanima Ranjan <tanimaranjan@gmail.com>
2019-06-18 12:06:03 -0400

    template use

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
+const {generateMessage} =require('./utils/messages')
 
 const app = express() 
 
@@ -9,31 +11,39 @@ const server = http.createServer(app)
 
 const io = socketio(server)
 
-let count = 0
-
 const port = process.env.PORT || 3000 
 const publicDir = path.join(__dirname , '../public')
 
 app.use(express.static(publicDir))
 
 io.on('connection', (socket) => {
-    console.log('New Socket connection')
-    socket.emit('message', 'Welcome!!!') 
+    // console.log('Server : New Socket connection')
+    socket.emit('message' , generateMessage('Welcome !'))
 
-    socket.on('sendMessage', (city) => {
-        console.log(`I got it ${city}`)
-        io.emit('message', city)
+    socket.broadcast.emit('message', generateMessage('New User has joined the chat'))
+
+    socket.on('sendMessage', (message, callback) => {
+
+        const filter = new Filter()
+
+        if(filter.isProfane(message)) {
+            return callback('Profanity is not allowed')
+        }
+       io.emit('message', generateMessage(message))
+        callback()
     })
 
-
-    // socket.emit('countUpdated', count)
-    // socket.on('increment', ()=> {
-    //     count++
-    //     // socket.emit('countUpdated', count)
-    //     io.emit('countUpdated', count)
-    // })
+    socket.on('shareLocation', (position, callback) => {
+        io.emit('locationMessage', 
+        generateMessage(`https://google.com/maps?q=${position.latitude},${position.longitude}`))
+        callback()
+    })
+    
+    socket.on('disconnect', () => {
+        console.log('Server : Someone disconnected')
+        io.emit('message', generateMessage('Someone left the chat'))
+    })
 })
-
 
 
 server.listen(port, () => console.log(`Server is up on port ${port}`));
