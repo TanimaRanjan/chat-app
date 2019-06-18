ba60a39c19bdc14b75d9d61a5e0c586095f1dba0
Tanima Ranjan <tanimaranjan@gmail.com>
2019-06-18 12:05:37 -0400

    Chat

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,26 +1,76 @@
 const socket = io()
 
-// socket.on('countUpdated', (count) => {
-//     console.log('Count has been update  '+count)
-// })
+
+
+// Elements
+const $messageForm = document.querySelector('#messageForm')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
+const $geoLocationButton = document.querySelector('#shareLocation')
+
+const $messageDisplay = document.querySelector('#message')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const urlTemplate = document.querySelector('#url-template').innerHTML
+
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message: message.text, 
+        createdAt:moment(message.createdAt).format('HH:mm')
+    })
+    $messageDisplay.insertAdjacentHTML('beforeend', html)
 })
 
 
-document.querySelector('#messageTag').addEventListener('submit', (e)=> {
+// Location Message 
+socket.on('locationMessage', (url) => {
+    console.log('Using Location message', url)
+    const html = Mustache.render(urlTemplate, {
+        url: url.text,
+        createdAt:moment(url.createdAt).format('HH.mm')
+    })    
+    $messageDisplay.insertAdjacentHTML('beforeend', html)
+})
+
+
+$messageForm .addEventListener('submit', (e)=> {
     e.preventDefault()
-    // console.log(e.target[0].value)
-    // document.querySelector('input').value    
-    // const city = e.target[0].value
-    const city = e.target.elements.message.value
-    socket.emit('sendMessage', city)
+    // disable
+    const message = e.target.elements.message.value
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    socket.emit('sendMessage', message, (error) => {
+        //enable 
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+        if(error) {
+            console.log('error')    
+        } else {
+            console.log(`Message was delivered! ${message}`)
+        }
+        
+    })
+    
 })
 
+$geoLocationButton.addEventListener('click', (e) => {
+    if(!navigator.geolocation) {
+        return alert('Geolocation is not supported')
+    } 
+    $geoLocationButton.setAttribute('disabled', 'disabled')
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('shareLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, 
+        () => {
+            console.log('Location shared')
+            $geoLocationButton.removeAttribute('disabled')
+        })
+    })
+})
 
-
-// document.querySelector('#increment').addEventListener(('click'), () => {
-//     // count ++;
-//     console.log('clicked')
-//     socket.emit('increment')
-// })
