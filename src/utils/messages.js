0dff6149b61418e5c9d2cf47adc10e162962e83b
TanimaRanjan <tanimaranjan@gmail.com>
2019-11-05 12:33:14 -0500

    Messages Js

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,11 +1,20 @@
-const generateMessage = (text) => {
+const generateMessage = (username, text) => {
     return { 
+        username,
         text, 
         createdAt: new Date().getTime()
     }
+}
 
+const generateLocationMessage = (username, url) => {
+    return { 
+        url: url, 
+        username:username,
+        createdAt: new Date().getTime()
+    }
 }
 
 module.exports = {
-    generateMessage
+    generateMessage,
+    generateLocationMessage
 }
